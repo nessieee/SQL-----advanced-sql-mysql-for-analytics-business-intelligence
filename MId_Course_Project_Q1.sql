efc1aca7d5a53e5a5b29794d912d6e4689489e16
nessieee <48605793+nessieee@users.noreply.github.com>
2020-05-11 22:20:21 +0200

    Add files via upload

---
+++ MId_Course_Project_Q1.sql
@@ -0,0 +1,20 @@
+USE mavenfuzzyfactory;
+
+SELECT 
+	-- YEAR(website_sessions.created_at),
+    -- MONTH(website_sessions.created_at),
+    MIN(DATE(website_sessions.created_at)) AS month_start,
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) AS orders,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate
+FROM website_sessions
+LEFT JOIN orders
+	ON website_sessions.website_session_id = orders.website_session_id
+WHERE
+website_sessions.utm_source = 'gsearch'
+	AND website_sessions.created_at < '2012-11-27'
+GROUP BY 
+	YEAR(website_sessions.created_at),
+    MONTH(website_sessions.created_at)
+	
+;
